a10cb46bf25be6a47f594f2aab844a135114dfd8
Muhammad Fuzail <muhammadfuzail@gmail.com>
2025-03-10 06:59:04 +0500

    Added button at home

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center w-full h-full mt-[50vh]">
+    <div className="flex items-center flex-col gap-10 justify-center w-full h-full mt-[50vh]">
       Hello to the code editor
+      <Link href="code-editor" className="p-4 font-bold text-black bg-slate-300">Code Editor</Link>
     </div>
   );
 }
